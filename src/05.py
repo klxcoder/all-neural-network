b53ebf41e8eb6be75b894bb2e82f6d07b0637735
klxcoder <klxcoder@gmail.com>
2025-04-06 13:47:23 +0700

    main

--- src/05.py
+++ src/05.py
@@ -3,8 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+def main():
     x = np.arange(10)
     _, axes = plt.subplots(1, 2, figsize=(12, 6))
-    axes[0].plot(x, x*x)
+    axes[0].plot(x, x * x)
     plt.show()
+if __name__ == "__main__":
+    main()
