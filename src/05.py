ac52bc31e704a5dfd1a76ec4a54f98562dac2397
klxcoder <klxcoder@gmail.com>
2025-04-06 14:09:09 +0700

    linear_regression

--- src/05.py
+++ src/05.py
@@ -8,15 +8,23 @@ def get_data():
     y = 3 * x + 5 + np.random.uniform(-1, 1, 100) * 5
     return x, y
 
+def linear_regression():
+    m = 0
+    b = 0
+    return m, b
+
 def main():
     x, y = get_data()
 
     _, axes = plt.subplots(1, 2, figsize=(12, 6))
 
-    axes[0].scatter(x, y)
+    axes[0].scatter(x, y, color='blue', label='Original data')
     axes[0].set_title('y = f(x)')
     axes[0].set_xlabel('x')
     axes[0].set_ylabel('y')
+    m, b = linear_regression()
+    axes[0].plot(x, m*x + b, color='red', label='Fitted line')
+    axes[0].legend()
 
     axes[1].set_title('loss')
     axes[1].set_xlabel('t')
