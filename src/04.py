8680e3b071285a81e875a3528de585a2bef1f0c2
klxcoder <klxcoder@gmail.com>
2025-04-06 16:01:49 +0700

    predict

--- src/04.py
+++ src/04.py
@@ -1,8 +1,12 @@
 from simple_load_linear_regression import simple_load_linear_regression
 from models import models
 from layers import layers
+import matplotlib.pyplot as plt
 
 x, y = simple_load_linear_regression()
+
+plt.scatter(x.flatten(), y)
+
 x = x.reshape((-1, 1))
 
 model = models.Sequential()
@@ -11,3 +15,8 @@ model.add(layers.Dense(1, 'softmax'))
 model.compile()
 
 model.fit(x, y)
+
+y_pred = model.layers[-1].neurons.flatten()
+plt.plot(x.flatten(), y_pred)
+
+plt.show()
