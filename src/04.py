c1bfee73c21c602d0ed5760a77af4c68adb3b2da
klxcoder <klxcoder@gmail.com>
2025-04-06 10:58:34 +0700

    add second model

--- src/04.py
+++ src/04.py
@@ -2,6 +2,6 @@ from models import models
 from layers import layers
 
 model = models.Sequential()
-dense = layers.Dense(10, 'relu')
-model.add(dense)
+model.add(layers.Dense(1, 'relu'))
+model.add(layers.Dense(1, 'softmax'))
 model.fit([1, 2, 3], [2, 4, 6])
