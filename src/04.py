861d0393782faf2138bb62de2d64300fc30082c8
klxcoder <klxcoder@gmail.com>
2025-04-06 16:05:48 +0700

    2 axes

--- src/04.py
+++ src/04.py
@@ -3,20 +3,30 @@ from models import models
 from layers import layers
 import matplotlib.pyplot as plt
 
-x, y = simple_load_linear_regression()
+def main():
+    x, y = simple_load_linear_regression()
+    x = x.reshape((-1, 1))
+    model = models.Sequential()
+    model.add(layers.Dense(1, 'relu'))
+    model.add(layers.Dense(1, 'softmax'))
+    model.compile()
 
-plt.scatter(x.flatten(), y)
+    _, axes = plt.subplots(1, 2, figsize=(12, 6))
+a
+    axes[0].scatter(x.flatten(), y, color='blue', label='Original data')
+    axes[0].set_title('y = f(x)')
+    axes[0].set_xlabel('x')
+    axes[0].set_ylabel('y')
+    model.fit(x, y)
+    y_pred = model.layers[-1].neurons.flatten()
+    axes[0].plot(x.flatten(), y_pred, color='red', label='Fitted line')
+    axes[0].legend()
 
-x = x.reshape((-1, 1))
+    axes[1].set_title('loss')
+    axes[1].set_xlabel('t')
+    axes[1].set_ylabel('loss')
+    # axes[1].plot(loss_history)
 
-model = models.Sequential()
-model.add(layers.Dense(1, 'relu'))
-model.add(layers.Dense(1, 'softmax'))
-model.compile()
-
-model.fit(x, y)
-
-y_pred = model.layers[-1].neurons.flatten()
-plt.plot(x.flatten(), y_pred)
-
-plt.show()
+    plt.show()
+if __name__ == "__main__":
+    main()
