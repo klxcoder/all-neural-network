8900196b7da55709bd6a9c937fd0b1a368dd17a3
klxcoder <klxcoder@gmail.com>
2025-04-06 15:03:31 +0700

    04.py use simple_load_linear_regression

--- src/04.py
+++ src/04.py
@@ -1,9 +1,14 @@
+from simple_load_linear_regression import simple_load_linear_regression
 from models import models
 from layers import layers
 import numpy as np
 
+x, y = simple_load_linear_regression()
+x = x.reshape((-1, 1))
+
 model = models.Sequential()
-model.add(layers.Dense(2, 'relu'))
-model.add(layers.Dense(3, 'softmax'))
+model.add(layers.Dense(1, 'relu'))
+model.add(layers.Dense(1, 'softmax'))
 model.compile()
-model.fit(np.random.uniform(-1, 1, (1, 2)), np.random.uniform(-1, 1, (1, 3)))
+
+model.fit(x, y)
