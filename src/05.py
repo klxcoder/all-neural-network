fd71483c3fe5bdb8f6beac8d6e16cef39465a2bb
klxcoder <klxcoder@gmail.com>
2025-04-06 13:50:29 +0700

    get_data

--- src/05.py
+++ src/05.py
@@ -3,10 +3,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def get_data():
+    x = np.arange(-10, 11)
+    y = 3*x*x + 2*x + 5
+    return x, y
+
 def main():
-    x = np.arange(10)
+    x, y = get_data()
+
     _, axes = plt.subplots(1, 2, figsize=(12, 6))
-    axes[0].plot(x, x * x)
+
+    axes[0].plot(x, y)
+    axes[0].set_title('y = f(x)')
+
     plt.show()
 if __name__ == "__main__":
     main()
