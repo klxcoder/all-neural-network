d233e121c15ac08a5cac6918d9a7d54f1f7d6ecc
klxcoder <klxcoder@gmail.com>
2025-04-06 15:22:05 +0700

    loss

--- src/models.py
+++ src/models.py
@@ -2,6 +2,10 @@ import numpy as np
 
 np.random.seed(1)
 
+def loss(y, y_pred):
+    mse = (1 / len(y)) * sum((y - y_pred) ** 2)
+    return  mse
+
 class Sequential:
     def __init__(self):
         self.x = None
@@ -12,7 +16,8 @@ class Sequential:
         if len(self.layers) < 2:
             raise ValueError("layers length must be at least 2")
         for layer_index in range(len(self.layers) - 1):
-            weight = np.random.uniform(-1, 1, (self.layers[layer_index].n, self.layers[layer_index+1].n))
+            # weight = np.random.uniform(-1, 1, (self.layers[layer_index].n, self.layers[layer_index+1].n))
+            weight = np.zeros((self.layers[layer_index].n, self.layers[layer_index + 1].n))
             self.weights.append(weight)
     def forward(self, x):
         self.layers[0].neurons = np.array(x)
@@ -27,8 +32,8 @@ class Sequential:
         self.x = x
         self.y = y
         self.forward(x)
-        print(y)
-        print(self.layers[-1].neurons)
+        l = loss(y, self.layers[-1].neurons.flatten())
+        print('loss = ', l)
     def add(self, layer):
         self.layers.append(layer)
 
